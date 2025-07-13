6877641fde1f30d959da2c0ae5528815258f0bce
ikguzzi <ikguzzi@hotmail.com>
2025-07-13 21:26:39 +0200

    Added fixed window size

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 
+debugSize = '1200x700'
+
 app = tk.Tk()
 
 paintTitle = 'Miniature Paint Catalog'
 app.title(paintTitle)
+app.geometry(debugSize)
 
 # Header
-header = tk.Label(app, text="Hello World!", font=('Arial', 14))
+header = tk.Label(app, text='Hello World!', font=('Arial', 14))
 header.pack(pady=20)
 
 app.mainloop()
