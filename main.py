9e378bceaec4f0a0418da3788104d6e022aee612
ikguzzi <ikguzzi@hotmail.com>
2025-07-14 18:14:28 +0200

    Added simple navbar for landing page

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import tkinter as tk
 
-debugSize = '1200x700'
+windowSize = '1200x700'
 
 app = tk.Tk()
 
 paintTitle = 'Miniature Paint Catalog'
 app.title(paintTitle)
-app.geometry(debugSize)
+app.geometry(windowSize)
 
 # Header
-header = tk.Label(app, text='Hello World!', font=('Arial', 14))
+header = tk.Label(app, text='Warhammer Paint Catalog', font=('Arial', 30))
 header.pack(pady=20)
+
+# Navbar
+
+btnWidth = 30
+
+collectionBtn = tk.Button(text='Paint Collection', width=btnWidth)
+collectionBtn.pack()
+
+schemesBtn = tk.Button(text='Paint Schemes', width=btnWidth)
+schemesBtn.pack()
 
 app.mainloop()
