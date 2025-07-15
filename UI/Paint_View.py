b77cd8cd70d8842bf93887ac5eda0d358ad71863
ikguzzi <ikguzzi@hotmail.com>
2025-07-15 20:25:43 +0200

    Added header label for new windows

--- UI/Paint_View.py
+++ UI/Paint_View.py
@@ -4,4 +4,6 @@ def Open_Paints_View():
     viewWin = tk.Toplevel()
     viewWin.title("Paint Inventory")
     viewWin.geometry("400x300")
-    
+
+    header = tk.Label(viewWin, text='Paint Catalog', font=('Arial', 30))
+    header.pack()
--- UI/Schemes_View.py
+++ UI/Schemes_View.py
@@ -4,3 +4,6 @@ def Open_Schemes_View():
     viewWin = tk.Toplevel()
     viewWin.title("Paint Schemes")
     viewWin.geometry("400x300")
+
+    header = tk.Label(viewWin, text='Paint Schemes', font=('Arial', 30))
+    header.pack()
