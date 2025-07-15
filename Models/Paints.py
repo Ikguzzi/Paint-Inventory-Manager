e80da7fc12d10628e7dd3be901bdbcc0818bf34e
ikguzzi <ikguzzi@hotmail.com>
2025-07-15 18:42:21 +0200

    Created the classes for paints and paint schemes

---
+++ Models/Paints.py
@@ -0,0 +1,8 @@
+class Paint():
+    def __init__(self, id, name, brand, type, value, status):
+        self.id = id
+        self.name = name
+        self.brand = brand
+        self.type = type
+        self.value = value
+        self.status = status
---
+++ Models/Schemes.py
@@ -0,0 +1,3 @@
+class Paint_Scheme():
+    def __init__(self, name):
+        self.name = name
